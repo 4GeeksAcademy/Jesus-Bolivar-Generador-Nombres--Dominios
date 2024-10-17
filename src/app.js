ae018f70b79846e2a8fa938f8415250c953860f4
yesus011 <jesu8907@gmail.com>
2024-10-17 16:32:55 +0000

    camios-de-lineamientos-de-código

--- src/app.js
+++ src/app.js
@@ -9,12 +9,15 @@ window.onload = function() {
   let pronoun = ["the", "our", "hola"];
   let adj = ["great", "big", "mundo"];
   let noun = ["jogger", "racoon", "soyJesus"];
+  let extensions = [".com", ".net", ".es"];
 
   for (let a = 0; a < pronoun.length; a++) {
     for (let b = 0; b < adj.length; b++) {
-      for (let c = 0; c < noun.length; c++) {
-        console.log(pronoun[a] + adj[b] + noun[c] + ".com");
-      }
+      for (let c = 0; c < noun.length; c++)
+        for (let d = 0; d < noun.length; d++) {
+          let result = pronoun[a] + adj[b] + noun[c] + extensions[d];
+          console.log(result);
+        }
     }
   }
 };
